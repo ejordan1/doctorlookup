b300ed78df6e0c07b0a462107e2b90802dd389c5
Student <example@example.com>
2019-07-05 16:14:26 -0700

    add book functionality, grabbing all info and displaying on turning pages

--- src/main.js
+++ src/main.js
@@ -9,49 +9,47 @@ import linesImage from './../img/Booklines.png';
 let lines = document.getElementById("lines");
 lines.src = linesImage;
 
+let clientDoctorArray = [];
+let currentPage = -1;
 
 var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&limit=1000&user_key=${process.env.exports.apiKey}`;
 // import 'bootstrap/dist/css/bootstrap.min.css';
 $(document).ready(function() {
+    $(".doctorCard").hide();
+    $(".endPage").hide();
+
   $("#nameForm").submit(function(event){
-
+    currentPage = -1;
     let nameInput = $("#name").val();
-
+    $(".bookTitle").text("The Doctor " + nameInput + " Book");
     let newUrl = resource_url + "&name=" + nameInput;
     console.log("newurl: " + newUrl);
-    // let long = parseFloat($("#userLong").val());
-    // let lat = parseFloat($("#userLat").val());
-    // // placeholder clouds
-    // let cloud_score = false;
-    // // placeholder date
-    //   // debugger;
-    //   //only works so far with 2014
-    // let date = $("#userDate").val().toString();
-    // let date2 = "1995-05-02";
-    // let dateStringGetter = `${date}`;
-    // if (date === dateStringGetter){
-    //   console.log("true");
-    // }
-    // let resolution = 0.8816 - parseFloat($("#resolutionRange").val());
     $.get(newUrl).then(function(response){
-      let a = response;
-      console.log(response);
-      displayDoctorsInfo(response);
 
+      console.log(response);
+      clientDoctorArray = response.data;
+
+      updatePage();
     }).fail(function(error){
       console.log("there was an error: ");
-
     })
     event.preventDefault();
   });
   $("#symptomsForm").submit(function(event){
+    currentPage = -1;
+    updatePage();
     let symptomsInput = $("#symptoms").val();
+    $(".bookTitle").text("The Doctors of " + symptomsInput + " Book");
     let newUrl = resource_url + "&query=" + symptomsInput;
     console.log("newurl: " + newUrl);
     $.get(newUrl).then(function(response){
       let a = response;
       console.log(response);
-      displayDoctorsInfo(response);
+      clientDoctorArray = response.data;
+      if (response.data.length >= 1){
+      displayDoctorsInfo(currentPage);
+      }
+      //else : NO DOCTORS WITH SEARCH PROVIDED
 
     }).fail(function(error){
       console.log("there was an error: ");
@@ -60,12 +58,79 @@ $(document).ready(function() {
 
     event.preventDefault();
   });
+  $(".rightArrow button").click(function(){
+
+    if (currentPage === clientDoctorArray.length){
+      alert("that was the last page");
+    } else {
+
+        currentPage++;
+    }
+    updatePage();
+  });
+  $(".leftArrow button").click(function(){
+
+    if (currentPage === -1){
+    } else {
+        currentPage--;
+    }
+    updatePage();
+  });
 });
 
-function displayDoctorsInfo(response){
-  let doctorArray = response.data;
-  doctorArray.forEach(function(doctor){
-    console.log(doctor.profile.name + ": " + doctor.profile.bio);
-  //  console.log(doctor.profile.practices.location_slug);
-  });
+function displayDoctorsInfo(index){
+  resetFields();
+
+    let doc = clientDoctorArray[index];
+    if (doc){
+      $("#card0 .docName").text(doc.profile.first_name + " " + doc.profile.last_name);
+      $("#card0 .docBio").text(doc.profile.bio)
+
+      $("#card0 .docAddress").text(doc.practices[0].visit_address.street + ", " + doc.practices[0].visit_address.city + ", " + doc.practices[0].visit_address.state + ", " + doc.practices[0].visit_address.zip);
+
+      $("#card0 .docPhone").text("Phone Number: " + doc.practices[0].phones[0].number)
+
+      $("#card0 .docWebsite").text("Website: " + doc.practices[0].website)
+
+      $("#card0 .docNewPatients").text("Accepts new patients: " +
+
+      doc.practices[0].accepts_new_patients)
+      //if (doc.profile.image_url){
+        console.log(doc.profile.image_url);
+        $("#card0 .pic").attr("src", doc.profile.image_url);
+      //}
+    }
+}
+
+function resetFields(){
+  $("#card0 .docName").text("no name provided");
+  $("#card0 .docBio").text("no bio provided");
+  $("#card0 .docAddress").text("no address provided");
+  $("#card0 .docPhone").text("no phone number provided");
+  $("#card0 .docNewPatients").text("no info on if accepting new patients");
+  $("#card0 .pic").attr("src", "");
+}
+function updatePage(){
+  if (currentPage === -1){
+    //display title page
+    $(".leftArrow").hide();
+    $(".rightArrow").show();
+    $(".doctorCard").hide();
+    $(".coverPage").show();
+    $(".endPage").hide();
+  } else if(currentPage === clientDoctorArray.length){
+    //display the end page
+    $(".leftArrow").show();
+    $(".rightArrow").hide();
+    $(".coverPage").hide();
+    $(".doctorCard").hide();
+    $(".endPage").show();
+  } else {
+    $(".rightArrow").show();
+    $(".leftArrow").show();
+    $(".doctorCard").show();
+    displayDoctorsInfo(currentPage);
+    $(".coverPage").hide();
+    $(".endPage").hide();
+  }
 }
