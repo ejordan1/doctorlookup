f3e4d60b2153a5554108c364af35272eda85aa40
Student <example@example.com>
2019-07-05 11:10:05 -0700

    add searching by name and query

--- src/main.js
+++ src/main.js
@@ -6,14 +6,15 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&user_key=' + exports.apiKey;
 
+var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&limit=1000&user_key=${process.env.exports.apiKey}`;
 // import 'bootstrap/dist/css/bootstrap.min.css';
-setInterval(function(){
-  console.log("a");
-}, 1000);
 $(document).ready(function() {
-  $("#userInputForm").submit(function(event){
+  $("#nameForm").submit(function(event){
+
+    let nameInput = $("#name").val();
+    console.log("nameInput: " + nameInput);
+    let newUrl = resource_url + "&name=" + nameInput;
     // let long = parseFloat($("#userLong").val());
     // let lat = parseFloat($("#userLat").val());
     // // placeholder clouds
@@ -28,8 +29,7 @@ $(document).ready(function() {
     //   console.log("true");
     // }
     // let resolution = 0.8816 - parseFloat($("#resolutionRange").val());
-
-    $.get(resource_url).then(function(response){
+    $.get(newUrl).then(function(response){
       let a = response;
       console.log(response);
 
@@ -37,18 +37,22 @@ $(document).ready(function() {
       console.log("there was an error: ");
 
     })
-    // $.get(`https://api.nasa.gov/planetary/earth/imagery/?lon=${long}&lat=${lat}&cloud_score=${cloud_score}&api_key=${process.env.API_KEY}&dim=${resolution}`).then(function(response){
-    //   $("#imageRow").html("<img src='" + `${response.url}` +"'</img>");
-    //   console.log(response.url);
-    //   console.log(response);
-    //   console.log(response.toString());
-    //   console.log("Success");
-    // }).fail(function(error){
-    //   $("#imageRow").html("<h1>There was an error processing your request. Please try a new location or date.</h1>");
-    //   console.log("Error!");
-    //   console.log(`${resolution}`);
-    // });
-    // Date nust be given in year-mm-dd
+    event.preventDefault();
+  });
+  $("#symptomsForm").submit(function(event){
+    let symptomsInput = $("#symptoms").val();
+    console.log("symptomsInput: " + symptomsInput);
+    let newUrl = resource_url + "&query=" + symptomsInput;
+
+    $.get(newUrl).then(function(response){
+      let a = response;
+      console.log(response);
+
+    }).fail(function(error){
+      console.log("there was an error: ");
+
+    })
+
     event.preventDefault();
   });
 });
